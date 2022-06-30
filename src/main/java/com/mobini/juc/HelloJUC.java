4761f195160fb8465deaa1de0cb297d77cf9169c
mobiwusihuan288 <mobiwusihuan288@163.com>
2022-06-30 19:32:04 +0800

    测试项目

---
+++ src/main/java/com/mobini/juc/HelloJUC.java
@@ -0,0 +1,12 @@
+package com.mobini.juc;
+
+/**
+ * @Description: 启动测试
+ * @Author: Mobini
+ * @Date: 2022-06-30
+ */
+public class HelloJUC {
+    public static void main(String[] args) {
+        System.out.println("HelloJUC");
+    }
+}
